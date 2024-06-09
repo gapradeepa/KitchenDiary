517fecaa4cce382db6259410d92fdab808b6dd39
Pradeepa <Pradeepa.ga@hotmail.com>
2024-06-09 10:47:34 -0300

    Modified to check the deployment

--- my-kitchen-diary/src/Navbar.js
+++ my-kitchen-diary/src/Navbar.js
@@ -4,79 +4,81 @@ import { Container } from "react-bootstrap";
 const Navbarmenu = () => {
   return (
     <Container>
-      {/* <nav
-        class="navbar navbar-expand-lg bg-primary fixed-top"
-        data-bs-theme="dark"
-      >
-        <div class="container-fluid">
-          <a class="navbar-brand" href="#">
-            Kitchen Diary
-          </a>
-          <button
-            class="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarColor01"
-            aria-controls="navbarColor01"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarColor01">
-            <ul class="navbar-nav me-auto">
-              <li class="nav-item">
-                <a class="nav-link active" href="#">
-                  Home
-                  <span class="visually-hidden">(current)</span>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">
-                  Features
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">
-                  Pricing
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">
-                  About
-                </a>
-              </li>
-              <li class="nav-item dropdown">
-                <a
-                  class="nav-link dropdown-toggle"
-                  data-bs-toggle="dropdown"
-                  href="#"
-                  role="button"
-                  aria-haspopup="true"
-                  aria-expanded="false"
-                >
-                  Dropdown
-                </a>
-                <div class="dropdown-menu">
-                  <a class="dropdown-item" href="#">
-                    Action
+      {
+        <nav
+          class="navbar navbar-expand-lg bg-primary fixed-top"
+          data-bs-theme="dark"
+        >
+          <div class="container-fluid">
+            <a class="navbar-brand" href="#">
+              Kitchen Diary
+            </a>
+            <button
+              class="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarColor01"
+              aria-controls="navbarColor01"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarColor01">
+              <ul class="navbar-nav me-auto">
+                <li class="nav-item">
+                  <a class="nav-link active" href="#">
+                    Home
+                    <span class="visually-hidden">(current)</span>
                   </a>
-                  <a class="dropdown-item" href="#">
-                    Another action
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">
+                    Features
                   </a>
-                  <a class="dropdown-item" href="#">
-                    Something else here
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">
+                    Pricing
                   </a>
-                  <div class="dropdown-divider"></div>
-                  <a class="dropdown-item" href="#">
-                    Separated link
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">
+                    About
                   </a>
-                </div>
-              </li>
-            </ul>
+                </li>
+                <li class="nav-item dropdown">
+                  <a
+                    class="nav-link dropdown-toggle"
+                    data-bs-toggle="dropdown"
+                    href="#"
+                    role="button"
+                    aria-haspopup="true"
+                    aria-expanded="false"
+                  >
+                    Dropdown
+                  </a>
+                  <div class="dropdown-menu">
+                    <a class="dropdown-item" href="#">
+                      Action
+                    </a>
+                    <a class="dropdown-item" href="#">
+                      Another action
+                    </a>
+                    <a class="dropdown-item" href="#">
+                      Something else here
+                    </a>
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" href="#">
+                      Separated link
+                    </a>
+                  </div>
+                </li>
+              </ul>
+            </div>
           </div>
-        </div>
-      </nav> */}
+        </nav>
+      }
     </Container>
   );
 };
